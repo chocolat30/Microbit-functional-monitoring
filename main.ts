e5ebe7393ac5e2b5b7393092801d991b96d9d9b3
chocolat30 <61612661+chocolat30@users.noreply.github.com>
2022-05-16 17:52:15 +0700

    Compass showArrows

--- main.ts
+++ main.ts
@@ -35,21 +35,36 @@ basic.showLeds(`
 basic.pause(2000)
 basic.clearScreen()
 let FuncAll = [
-    ()=>{
+    () => {
         whaleysans.showNumber(input.temperature())
     },
-    ()=>{
-        if (input.compassHeading() > 99){
-            basic.showNumber(input.compassHeading())
-        } else {
-            whaleysans.showNumber(input.compassHeading())
+    () => {
+        let deg = input.compassHeading()
+        if (deg < 23 && deg > 0) {
+            basic.showArrow(ArrowNames.North)
+        } else if (deg < 68 && deg > 23){
+            basic.showArrow(ArrowNames.NorthWest)
+        } else if (deg < 113 && deg > 68 ) {
+            basic.showArrow(ArrowNames.West)
+        } else if (deg < 158 && deg > 113) {
+            basic.showArrow(ArrowNames.SouthWest)
+        } else if (deg < 203 && deg > 158) {
+            basic.showArrow(ArrowNames.South)
+        } else if (deg < 248 && deg > 203) {
+            basic.showArrow(ArrowNames.SouthEast)
+        } else if (deg < 293 && deg > 248) {
+            basic.showArrow(ArrowNames.East)
+        } else if (deg < 338 && deg > 293) {
+            basic.showArrow(ArrowNames.NorthEast)
+        } else if (deg < 360 && deg > 338) {
+            basic.showArrow(ArrowNames.North)
         }
     }
-    ]
+]
 basic.forever(function () {
     // add function here
     if (Funcs > 0) {
-    	FuncAll[Funcs-1]()
+        FuncAll[Funcs - 1]()
     } else {
         basic.showLeds(`
             . . # . .
